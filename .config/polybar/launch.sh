316f8b861fb43e96dbb7feedc0dd02bbad49b206
Sajfli <sajmon88@protonmail.ch>
2022-02-08 01:00:36 +0100

    Some improvements for my desktop

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,5 +5,13 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
+if type "xrandr"; then
+	  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		      MONITOR=$m polybar --reload mybar &
+		        done
+		else
+			  polybar --reload mybar &
+fi
+
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
+#polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
